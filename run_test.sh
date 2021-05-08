4e481efb6d3cc45dc89f8ba7cedc517de7ad8d36
ivansoya <ssoya.ivan@gmail.com>
2021-05-09 02:28:55 +0300

    Lab6: Редактирование задачи #7

--- run_test.sh
+++ run_test.sh
@@ -9,23 +9,24 @@ test_server_build()
 {
     g++ server.cpp -o server
     build_status=$?
-    #assertTrue "Build failed" "[ $build_status -eq 0 ]"
+    assertTrue "Build failed" "[ $build_status -eq 0 ]"
+    echo "Completed!"
 }
 
 test_fail_server1()
 {
     ./server "Fdwd" 
     return_status=$?
-    #assertTrue "Application test failed" "[ $return_status -eq 1 ]"
-    #assertTrue "Port is Fdwd" "[ $return_status -eq 1 ]"
+    assertTrue "Application test failed" "[ $return_status -eq 1 ]"
+    assertTrue "Port is Fdwd" "[ $return_status -eq 1 ]"
 }
 
 test_fail_server2()
 {
     ./server "3425" 2523
     return_status=$?
-    #assertTrue "Application test failed" "[ $return_status -eq 1 ]"
-    #assertTrue "Number of arguments bigger than 1!" "[ $return_status -eq 1 ]"
+    assertTrue "Application test failed" "[ $return_status -eq 1 ]"
+    assertTrue "Number of arguments bigger than 1!" "[ $return_status -eq 1 ]"
 }
 
 test_server_configuration()
@@ -50,21 +51,21 @@ test_server_configuration()
         fi
     else
     	echo "No input data found. Did you mess up with the code?"
-        #fail "No input data found. Did you mess up with the code?"
+        fail "No input data found. Did you mess up with the code?"
     fi
 }
 
 
 
 
-#if [[ -d "shunit2-2.1.8" ]]
-#then
-#    echo "Found shunit2-2.1.8"
-#else
-#    echo "Downloading shunit2-2.1.8 ..."
-#    curl https://codeload.github.com/kward/shunit2/tar.gz/v2.1.8 --output shunit2-2.1.8.tar.gz
-#    tar -xzf shunit2-2.1.8.tar.gz
-#fi
+if [[ -d "shunit2-2.1.8" ]]
+then
+    echo "Found shunit2-2.1.8"
+else
+    echo "Downloading shunit2-2.1.8 ..."
+    curl https://codeload.github.com/kward/shunit2/tar.gz/v2.1.8 --output shunit2-2.1.8.tar.gz
+    tar -xzf shunit2-2.1.8.tar.gz
+fi
 # load shunit2
-#. shunit2-2.1.8/shunit2
+. shunit2-2.1.8/shunit2
 
