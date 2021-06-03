3e0a31b897eaf3fe5664c435fcc4f86b487f2e0b
Иван Соя <ivansoa@MacBook-Pro-Ivan.local>
2021-06-03 15:56:39 +0300

    Deploy

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 ps cax | grep server > output.txt
-if [ -s output.txt ]; then
+if [ -s output.txt ]
+then
     echo "There no processes"
 else
-    killall server
+    sudo killall server
 fi
 rm output.txt
